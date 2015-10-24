4f6d8c3df8dcb39e9ae99b191174e806d54615c5
lxp <252319634@qq.com>
2015-10-25 05:21:51 +0800

    修改了mail设置

--- vmaig_blog/settings.py
+++ vmaig_blog/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'vmaig_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -110,87 +110,87 @@ AUTH_USER_MODEL = "vmaig_auth.VmaigUser"
 
 
 #log配置###########################################
-LOG_FILE = "./all.log"
-
-LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': True,
-
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-                }
-            },
-        'formatters': {
-            'simple': {
-                'format': '[%(levelname)s] %(module)s : %(message)s'
-                },
-            'verbose': {
-                'format': '[%(asctime)s] [%(levelname)s] %(module)s : %(message)s'
-                }
-            },
-
-        'handlers': {
-            'null': {
-                'level': 'DEBUG',
-                'class': 'django.utils.log.NullHandler',
-                },
-            'console': {
-                'level': 'INFO',
-                'class': 'logging.StreamHandler',
-                'formatter': 'verbose'
-                },
-            'file': {
-                'level': 'INFO',
-                'class': 'logging.FileHandler',
-                'formatter': 'verbose',
-                'filename': LOG_FILE,
-                'mode': 'a',
-                },
-            'mail_admins': {
-                'level': 'ERROR',
-                'class': 'django.utils.log.AdminEmailHandler',
-                'filters': ['require_debug_false']
-                }
-            },
-        'loggers': {
-            '': {
-                'handlers': ['file', 'console'],
-                'level': 'INFO',
-                'propagate': True,
-                },
-            'django': {
-                'handlers': ['file', 'console'],
-                'level': 'DEBUG',
-                'propagate': True,
-                },
-            'django.request': {
-                'handlers': ['mail_admins', 'console'],
-                'level': 'ERROR',
-                'propagate': True,
-                },
-            }
-        }
+# LOG_FILE = "./all.log"
+#
+# LOGGING = {
+#         'version': 1,
+#         'disable_existing_loggers': True,
+#
+#         'filters': {
+#             'require_debug_false': {
+#                 '()': 'django.utils.log.RequireDebugFalse'
+#                 }
+#             },
+#         'formatters': {
+#             'simple': {
+#                 'format': '[%(levelname)s] %(module)s : %(message)s'
+#                 },
+#             'verbose': {
+#                 'format': '[%(asctime)s] [%(levelname)s] %(module)s : %(message)s'
+#                 }
+#             },
+#
+#         'handlers': {
+#             'null': {
+#                 'level': 'DEBUG',
+#                 'class': 'django.utils.log.NullHandler',
+#                 },
+#             'console': {
+#                 'level': 'INFO',
+#                 'class': 'logging.StreamHandler',
+#                 'formatter': 'verbose'
+#                 },
+#             'file': {
+#                 'level': 'INFO',
+#                 'class': 'logging.FileHandler',
+#                 'formatter': 'verbose',
+#                 'filename': LOG_FILE,
+#                 'mode': 'a',
+#                 },
+#             'mail_admins': {
+#                 'level': 'ERROR',
+#                 'class': 'django.utils.log.AdminEmailHandler',
+#                 'filters': ['require_debug_false']
+#                 }
+#             },
+#         'loggers': {
+#             '': {
+#                 'handlers': ['file', 'console'],
+#                 'level': 'INFO',
+#                 'propagate': True,
+#                 },
+#             'django': {
+#                 'handlers': ['file', 'console'],
+#                 'level': 'DEBUG',
+#                 'propagate': True,
+#                 },
+#             'django.request': {
+#                 'handlers': ['mail_admins', 'console'],
+#                 'level': 'ERROR',
+#                 'propagate': True,
+#                 },
+#             }
+#         }
 
 
 #cache配置#########################################
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-snowflake',
-        'options': {
-            'MAX_ENTRIES': 1024,
-        }
-    },
-    'memcache': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        #'LOCATION': 'unix:/home/billvsme/memcached.sock',
-        'LOCATION': '127.0.0.1:11211',
-        'options': {
-            'MAX_ENTRIES': 1024,
-        }
-    },
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION': 'unique-snowflake',
+#         'options': {
+#             'MAX_ENTRIES': 1024,
+#         }
+#     },
+#     'memcache': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         #'LOCATION': 'unix:/home/billvsme/memcached.sock',
+#         'LOCATION': '127.0.0.1:11211',
+#         'options': {
+#             'MAX_ENTRIES': 1024,
+#         }
+#     },
+# }
 
 
 #添加templates 文件夹
@@ -203,15 +203,15 @@ TEMPLATE_DIRS = (
 PAGE_NUM = 3
 
 #email配置#########################################
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.qq.com'                       #SMTP地址 例如: smtp.163.com
-EMAIL_PORT = 25                       #SMTP端口 例如: 25
-EMAIL_HOST_USER = '252319634@qq.com'                  #我自己的邮箱 例如: xxxxxx@163.com
-EMAIL_HOST_PASSWORD = 'xp830815'              #我的邮箱密码 例如  xxxxxxxxx
-EMAIL_SUBJECT_PREFIX = u'django'       #为邮件Subject-line前缀,默认是'[django]'
-EMAIL_USE_TLS = True                  #与SMTP服务器通信时，是否启动TLS链接(安全链接)。默认是false
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.qq.com'                       #SMTP地址 例如: smtp.163.com
+# EMAIL_PORT = 25                       #SMTP端口 例如: 25
+# EMAIL_HOST_USER = '252319634@qq.com'                  #我自己的邮箱 例如: xxxxxx@163.com
+# EMAIL_HOST_PASSWORD = 'xp830815'              #我的邮箱密码 例如  xxxxxxxxx
+# EMAIL_SUBJECT_PREFIX = u'django'       #为邮件Subject-line前缀,默认是'[django]'
+# EMAIL_USE_TLS = True                  #与SMTP服务器通信时，是否启动TLS链接(安全链接)。默认是false
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 #七牛配置#######################################
 qiniu_access_key = ''
