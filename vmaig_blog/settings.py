e6adf120c5db3ede0f40a1bff1442620653da32b
lxp <252319634@qq.com>
2015-10-25 05:23:23 +0800

    修改了mail设置

--- vmaig_blog/settings.py
+++ vmaig_blog/settings.py
@@ -174,23 +174,23 @@ AUTH_USER_MODEL = "vmaig_auth.VmaigUser"
 
 
 #cache配置#########################################
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-#         'LOCATION': 'unique-snowflake',
-#         'options': {
-#             'MAX_ENTRIES': 1024,
-#         }
-#     },
-#     'memcache': {
-#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#         #'LOCATION': 'unix:/home/billvsme/memcached.sock',
-#         'LOCATION': '127.0.0.1:11211',
-#         'options': {
-#             'MAX_ENTRIES': 1024,
-#         }
-#     },
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+        'options': {
+            'MAX_ENTRIES': 1024,
+        }
+    },
+    'memcache': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        #'LOCATION': 'unix:/home/billvsme/memcached.sock',
+        'LOCATION': '127.0.0.1:11211',
+        'options': {
+            'MAX_ENTRIES': 1024,
+        }
+    },
+}
 
 
 #添加templates 文件夹
